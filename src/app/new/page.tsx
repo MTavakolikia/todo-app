8fa23c545711f7765c9d7bb516ca6e3213c76821
mohammadtk1994 <mohammadtavakolikia66@gmail.com>
2023-06-28 10:56:38 +0330

    from and todo list in now functional

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -6,9 +6,10 @@ const page = () => {
     async function createTodo(data: FormData) {
         "use server"
         const title = data.get('title')?.valueOf();
-        if (typeof title !== "string" || title.length === 0) {
+        if (typeof title !== 'string' || title.length === 0) {
             throw new Error("Invalid Title")
         }
+
         await prisma.todo.create({ data: { title, complete: false } })
         redirect("/")
     }
@@ -18,10 +19,11 @@ const page = () => {
                 <h1 className="text-2xl">New</h1>
             </header>
             <form className="flex gap-2 flex-col" action={createTodo}>
-                <input type="text"
-                    className="border border-slate-300 text-slate-300 bg-transparent px-2 py-1 rounded focus-within:bg-slate-100 outline-none" />
+                <input type="text" name="title"
+                    className="border border-slate-300 text-slate-300 bg-transparent px-2 py-1 rounded focus-within:bg-slate-900 outline-none"
+                />
                 <div className="flex justify-end gap-1">
-                    <Link href='/'
+                    <Link href='..'
                         className="border border-slate-300 text-slate-300 px-2 mx-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline">
                         Cancel
                     </Link>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,13 @@ function getTodos() {
   return prisma.todo.findMany();
 }
 
+async function toggleTodo(id: string, complete: boolean) {
+  "use server"
+  await prisma.todo.update({ where: { id }, data: { complete } })
+}
 export default async function Home() {
   const todos = await getTodos();
-  // await prisma.todo.create({ data: { title: "test", complete: false } })
+
   return (
     <>
       <header className="flex justify-between items-center mb-4">
@@ -17,7 +21,7 @@ export default async function Home() {
       </header>
       <ul className="pl-4">
         {todos.map(item => (
-          <TodoItem key={item.id} {...item} />
+          <TodoItem key={item.id} {...item} toggleTodo={toggleTodo} />
         ))}
       </ul>
     </>
