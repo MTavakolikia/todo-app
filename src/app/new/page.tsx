ff95c15a5ee1f61651b8f3dcc87a8f45e3e7f599
mohammadtk1994 <mohammadtavakolikia66@gmail.com>
2023-06-28 10:25:30 +0330

    TodoItem compnent created and added to main page for showing list of todos

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import { prisma } from "@/db"
+import { redirect } from "next/navigation";
+import Link from "next/link"
 
 const page = () => {
+    async function createTodo(data: FormData) {
+        "use server"
+        const title = data.get('title')?.valueOf();
+        if (typeof title !== "string" || title.length === 0) {
+            throw new Error("Invalid Title")
+        }
+        await prisma.todo.create({ data: { title, complete: false } })
+        redirect("/")
+    }
     return (
-        <div>page</div>
+        <>
+            <header className="flex justify-between items-center mb-4">
+                <h1 className="text-2xl">New</h1>
+            </header>
+            <form className="flex gap-2 flex-col" action={createTodo}>
+                <input type="text"
+                    className="border border-slate-300 text-slate-300 bg-transparent px-2 py-1 rounded focus-within:bg-slate-100 outline-none" />
+                <div className="flex justify-end gap-1">
+                    <Link href='/'
+                        className="border border-slate-300 text-slate-300 px-2 mx-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline">
+                        Cancel
+                    </Link>
+                    <button
+                        className="border border-slate-300 text-slate-300 mx-2 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline">
+                        Create
+                    </button>
+                </div>
+            </form>
+        </>
     )
 }
 
